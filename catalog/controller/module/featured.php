8f6347eb24d20c453faf8a6be17e38a8b5a898f2
Gaurav Mehra <gauravmehra1987@gmail.com>
2016-01-17 12:11:08 +0530

    featured.php updated

--- catalog/controller/module/featured.php
+++ catalog/controller/module/featured.php
@@ -60,7 +60,7 @@ class ControllerModuleFeatured extends Controller {
 
 					$data['products'][] = array(
 						'product_id'  => $product_info['product_id'],
-						'thumb'       => $image,
+						'thumb'       => $product_info['image'],
 						'name'        => $product_info['name'],
 						'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..',
 						'price'       => $price,
